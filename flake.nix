59132ae403cde18b5071685897cc298a9de214bc
KZdkm <tinghan.zhu@hotmail.com>
2025-05-19 20:57:03 +0800

    add nix

---
+++ flake.nix
@@ -0,0 +1,90 @@
+
+{
+  description = "Hedge";
+
+  inputs = {
+    systems = {
+      type = "github";
+      owner = "nix-systems";
+      repo = "default-linux";
+    };
+    hyprland = {
+      owner = "hyprwm";
+      repo = "Hyprland";
+      type = "github";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = {
+    self,
+    systems,
+    hyprland,
+    ...
+  }: let
+    inherit (builtins) concatStringsSep elemAt head readFile split substring;
+    inherit (hyprland.inputs) nixpkgs;
+
+    perSystem = attrs:
+      nixpkgs.lib.genAttrs (import systems) (system:
+        attrs system (import nixpkgs {
+          inherit system;
+          overlays = [hyprland.overlays.hyprland-packages];
+        }));
+
+    # Generate version
+    mkDate = longDate: (concatStringsSep "-" [
+      (substring 0 4 longDate)
+      (substring 4 2 longDate)
+      (substring 6 2 longDate)
+    ]);
+
+    version =
+      (head (split "'"
+        (elemAt
+          (split " version: '" (readFile ./meson.build))
+          2)))
+      + "+date=${mkDate (self.lastModifiedDate or "19700101")}_${self.shortRev or "dirty"}";
+  in {
+    packages = perSystem (system: pkgs: {
+      Hedge = let
+        hyprlandPkg = hyprland.packages.${system}.hyprland;
+      in
+        pkgs.gcc14Stdenv.mkDerivation {
+          pname = "Hedge";
+          inherit version;
+          src = ./.;
+
+          nativeBuildInputs = hyprlandPkg.nativeBuildInputs;
+          buildInputs = [hyprlandPkg] ++ hyprlandPkg.buildInputs;
+          dontUseCmakeConfigure = true;
+
+          meta = with pkgs.lib; {
+            homepage = "https://github.com/KZDKM/Hedge";
+            description = "Hot edge plugin for hyprland";
+            license = licenses.gpl2Only;
+            platforms = platforms.linux;
+          };
+        };
+      default = self.packages.${system}.Hedge;
+    });
+
+    devShells = perSystem (system: pkgs: {
+      default = pkgs.mkShell {
+        name = "Hedge-shell";
+        nativeBuildInputs = with pkgs; [gcc14 clang-tools bear];
+        buildInputs = [hyprland.packages.${system}.hyprland];
+        inputsFrom = [
+          hyprland.packages.${system}.hyprland
+          self.packages.${system}.Hedge
+        ];
+        shellHook = ''
+          meson setup build --reconfigure
+          sed -e 's/c++23/c++2b/g' ./build/compile_commands.json > ./compile_commands.json
+        '';
+      };
+    });
+
+    formatter = perSystem (_: pkgs: pkgs.alejandra);
+  };
+}
